80f682e744cc9b0e6f6d1a0c81ae409104c64d82
pookieofdoom <pookieofdoom@gmail.com>
2016-11-29 23:08:52 -0800

    Initial Project

---
+++ src/NBACreateTable.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+import java.sql.*;
+
+public class NBACreateTable
+{
+   private ArrayList<String> mTableNames;
+   private Connection mConn;
+   
+   public NBACreateTable(Connection conn)
+   {
+      mConn = conn;
+      mTableNames = new ArrayList<String>();
+      mTableNames.add("Positions");
+      mTableNames.add("Coaches");
+      mTableNames.add("Teams");
+      mTableNames.add("Players");
+      mTableNames.add("Stats");
+      
+   }
+   
+   public ArrayList<String> getTableNames()
+   {
+      return mTableNames;
+   }
+   
+   public void createTables(String tableName)
+   {
+      try
+      {
+         Statement s1 = mConn.createStatement();
+         String table = "CREATE TABLE " + tableName;
+         
+         if (tableName.equals("Positions"))
+         {
+            table = table + "(Pos INT,"
+                          + "Position VARCHAR(16),"
+                          + "PRIMARY KEY (Pos)"
+                          + ")";
+         }
+         
+         
+         else if (tableName.equals("Coaches"))
+         {
+            table = table + "(ID INT,"
+                          + "FirstName VARCHAR(16),"
+                          + "LastName VARCHAR(16),"
+                          + "PRIMARY KEY (Id)"
+                          + ")";
+         }
+         
+         else if (tableName.equals("Teams"))
+         {
+            table = table + "(ID INT,"
+                          + "Name VARCHAR(12),"
+                          + "State CHAR(2),"
+                          + "City VARCHAR(16),"
+                          + "CoachID INT,"
+                          + "Abbrev VARCHAR(3),"
+                          + "PRIMARY KEY (ID),"
+                          + "FOREIGN KEY (CoachId) REFERENCES Coaches(Id)"
+                          + ")";
+         }
+         
+         else if (tableName.equals("Players"))
+         {
+            table = table + "(ID INT,"
+                          + "FirstName VARCHAR(16),"
+                          + "LastName VARCHAR(16),"
+                          + "TeamID INT,"
+                          + "Position INT,"  
+                          + "Height INT,"
+                          + "Weight INT,"
+                          + "ShootingHand CHAR(1),"
+                          + "PRIMARY KEY (Id),"
+                          + "FOREIGN KEY (TeamId) REFERENCES Teams(ID),"
+                          + "FOREIGN KEY (Position) REFERENCES Positions(Pos)"
+                          + ")";
+         }
+         
+         else if (tableName.equals("Stats"))
+         {
+            table = table + "(PlayerId INT,"
+                          + "Season INT,"
+                          + "Age INT,"
+                          + "TeamId INT,"
+                          + "Points INT,"
+                          + "Assists INT,"
+                          + "Rebounds INT,"
+                          + "Steals INT,"
+                          + "Blocks INT,"
+                          + "FGM INT,"
+                          + "FGA INT,"
+                          + "3PM INT,"
+                          + "3PA INT,"
+                          + "FTM INT,"
+                          + "FTA INT,"
+                          + "TurnOver INT,"
+                          + "PRIMARY KEY(PlayerId, Season, TeamId),"
+                          + "FOREIGN KEY(PlayerId) REFERENCES Players(ID),"
+                          + "FOREIGN KEY(TeamId) REFERENCES Teams(ID)"
+                          + ")";
+         }
+         
+         
+         System.out.println(table);
+         s1.executeUpdate(table);
+         
+         
+      } 
+      catch (SQLException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      
+   }
+}
---
+++ src/TestMysql.java
@@ -0,0 +1,130 @@
+import java.sql.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class TestMysql
+{
+   private static Connection conn;
+   private static String     url = "jdbc:mysql://";
+   private static String     host;
+   private static String     userid;
+   private static String     password;
+
+   public static void main(String args[])
+   {
+      NBACreateTable createTable = null;
+      //get database config
+      loadConfig();
+      
+      try
+      {
+         Class.forName("com.mysql.jdbc.Driver").newInstance();
+      }
+      catch (Exception e)
+      {
+         System.out.println("Driver not found");
+         System.out.println(e);
+      }
+      
+      conn = null;
+      try
+      {
+         conn = DriverManager.getConnection(url+host+"/"+userid+"?user="+userid+"&password="+password+"&");
+         createTable = new NBACreateTable(conn);
+      }
+      catch (Exception e)
+      {
+         System.out.println("Could not open connection");
+         System.out.println(e);
+      }
+      System.out.println("Connected");
+      
+      try
+      {
+         DatabaseMetaData dbm = conn.getMetaData();
+         ArrayList<String> tableNames = createTable.getTableNames();
+         boolean flag = false;
+         for (int i = 0; i < tableNames.size(); i++)
+         {
+            ResultSet tables = dbm.getTables(null, null, tableNames.get(i), null);
+            flag = false;
+            while (tables.next()) 
+            { 
+               String tName = tables.getString("TABLE_NAME");
+               if (tName != null && tName.equals(tableNames.get(i))) 
+               {
+                  System.out.println(tableNames.get(i));
+                  flag = true;
+                  break;
+               }
+           }
+            if (flag == false)
+            {
+               createTable.createTables(tableNames.get(i));
+            }
+         }
+         
+      } 
+      catch (SQLException e1)
+      {
+         // TODO Auto-generated catch block
+         e1.printStackTrace();
+      }
+      
+      
+      
+      try
+      {
+         conn.close();
+      }
+      catch (Exception e)
+      {
+         System.out.println("Unable to close Connection");
+      }
+      System.out.println("Connection Closed");
+     
+   }
+   
+   public static void loadConfig()
+   {
+      Properties prop = new Properties();
+      InputStream input = null;
+      try
+      {
+
+         input = new FileInputStream("dbconf.365");
+
+         // load a properties file
+         prop.load(input);
+
+         // get the property value
+         host = prop.getProperty("host");
+         userid = prop.getProperty("userid");
+         password = prop.getProperty("password");
+
+      } 
+      catch (IOException ex)
+      {
+         ex.printStackTrace();
+      } 
+      finally
+      {
+         if (input != null)
+         {
+            try
+            {
+               input.close();
+            } 
+            catch (IOException e)
+            {
+               e.printStackTrace();
+            }
+         }
+      }
+
+      
+   }
+}
